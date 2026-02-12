87f5e9dda51ff4f594c98aa636763a2dc727bc6c
GGiooni <boggioni.nicolas@gmail.com>
2026-02-12 17:40:08 +0000

    Show alert when task completed

--- app/script.js
+++ app/script.js
@@ -4,4 +4,5 @@ console.log("Task Manager loaded");
 // Listen for Complete Task button click
 document.getElementById("completeBtn").addEventListener("click", function() {
     alert("Task completed! Notification sent.");
+    document.getElementById("message").innerText = "Task completed! Notification sent.";
 });
